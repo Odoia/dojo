76de664708dc5aaee88e92eef6430801123aa812
Marcos Carvalho <marcos.carvalho@pipefy.com>
2021-09-24 15:56:12 -0300

    lots of prys

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,16 +2,19 @@ class UserController < ApplicationController
 
   def create
     user = params[:user]
-    
+
     result = User.new.tap do |u|
-      binding.pry
-      u.birth_date = Date.parse(user[:birth_date]).strftime('%d/%m/%Y')
+      u.birth_date = user[:birth_date]
       u.name = user[:name]
       u.city = user[:city]
       u.save
     end
 
+    binding.pry
 
+    result_as_json = JSON.parse(result)
+
+    binding.pry
     render :json => { data: result.as_json }, :status => 201
   end
 end
