905c1d27521840858995b3d79cf6ded794575a05
Marcos Carvalho <marcos.carvalho@pipefy.com>
2021-09-24 15:21:25 -0300

    deleted api folder and updated migration file

--- db/migrate/20210924180555_create_user.rb
+++ db/migrate/20210924180555_create_user.rb
@@ -1,7 +1,9 @@
 class CreateUser < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string 
+      t.string :name, :city
+      t.string :birth_date
+      t.timestamps
     end
   end
 end
