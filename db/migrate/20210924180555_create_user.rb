cbac7f3d95c9510b514f64609af25e6fa532799a
Hiago Borges <hiago.borges@pipefy.com>
2021-09-24 15:35:32 -0300

    Hiago

--- db/migrate/20210924180555_create_user.rb
+++ db/migrate/20210924180555_create_user.rb
@@ -2,7 +2,7 @@ class CreateUser < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name, :city
-      t.string :birth_date
+      t.date :birth_date
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_180555) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "city"
-    t.string "birth_date"
+    t.date "birth_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
