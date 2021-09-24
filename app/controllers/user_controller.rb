ced041130079ea0173fd5ab27345604c6d0fde0f
Tan Pipefy 🔥 <tania.zuniga@pipefy.com>
2021-09-24 13:00:25 -0500

    More test bonitos

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,6 @@ class UserController < ApplicationController
 
   def create
 
-    render :json => {data: {} }
+    render :json => { data: { id: 1, name: 'valid name', city: 'valid city', birth_date: '10/10/1991'} }, :status => 201
   end
 end
